4d2690f2167ff9c6b6afcf929c4337837f054844
Pedro Rosa <92191060+PedroBRosa@users.noreply.github.com>
2022-05-11 22:02:54 -0300

    adicionado as funções
    falta adicionar o JOp

--- Pr-tica-5-_LP/src/Ex2/Main.java
+++ Pr-tica-5-_LP/src/Ex2/Main.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        ArrayList<String> usuario =  new ArrayList<>();
+        ArrayList<String> usuario = new ArrayList<>();
         int menu = 0;
-        String nome,telefone,user,eMail;
+        String nome, telefone, user, eMail;
         while (menu != 4) {
             System.out.println("===================  OPÇÕES  =================");
             System.out.println("1 - Cadastrar ");
@@ -21,7 +21,7 @@ public class Main {
             menu = ler.nextInt();
             switch (menu) {
                 case 1:
-                    System.out.println("============= CADASTRAR PASSAGEIRO =============");
+                    System.out.println("============= CADASTRAR =============");
                     System.out.println("Escreva seu nome");
                     nome = ler.next();
                     System.out.println("Escreva seu telefone");
@@ -30,17 +30,41 @@ public class Main {
                     user = ler.next();
                     System.out.println("Escreva seu email");
                     eMail = ler.next();
-                    usuario.add(nome+" "+telefone+" "+user+" "+eMail);
+                    usuario.add(nome + " " + telefone + " " + user + " " + eMail);
                     break;
                 case 2:
-                    System.out.println("============= CHECK IN =============");
+                    System.out.println("============= EXCLUIR =============");
+                    System.out.println("Escreva o user que deseja excluir: ");
+                    user = ler.next();
+                    int indice = -1;
+                    for (int i = 0; i < usuario.size(); i++) {
+                        if (usuario.get(i).contains(user)) indice = i;
+                    }
+                    if (indice == -1) {
+                        System.out.println("Usuario não existe");
+                    }else{
+                        usuario.remove(indice);
+                    }
                     break;
                 case 3:
-                    System.out.println("============= CANCELAR VOO =============");
+                    System.out.println("============= PESQUISAR =============");
+                    System.out.println("Escreva o user que deseja pesquisar: ");
+                    user = ler.next();
+                    int j = -1;
+                    for (int i = 0; i < usuario.size(); i++) {
+                        if (usuario.get(i).contains(user)) j = i;
+                    }
+                    if (j == -1) {
+                        System.out.println("Usuario não existe");
+                    }else{
+                        System.out.println(usuario.get(j));
+                    }
                     break;
                 case 4:
+                    System.out.println("Sistema encerrado");
                     break;
                 default:
+                    System.out.println("Opção invalida");
                     break;
 
             }
